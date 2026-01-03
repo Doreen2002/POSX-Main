0f1a78e93d038a5ad5c2ab852d59ec83951a7bd2
Doreen2002 <doreenmwapekatebe8@gmail.com>
2026-01-03 15:34:31 +0200

    Fix update version 22

--- lib/widgets_components/side_bar.dart
+++ lib/widgets_components/side_bar.dart
@@ -259,7 +259,7 @@ Widget SideBar(BuildContext context, CartItemScreenController model) {
               Padding(
                 padding: EdgeInsets.only(bottom: 8.h),
                 child: SingleText(
-                  text: 'v1.0.21',
+                  text: 'v1.0.22',
                   fontSize: 4.sp,
                   color: Colors.grey,
                 ),
